937591d318762defb951364ffa6090d4102f0402
Vasilev <Lyubomir.Vasilev@adesso.bg>
2022-03-10 13:26:11 +0100

    Header component implement
    Navbar with the home,favourites and search options was implemented into
    the Header component.
    
    Access to the favorites route will be user restricted later.
    
    Please enter the commit message for your changes. Lines starting

--- src/components/Layout/Header/Header.js
+++ src/components/Layout/Header/Header.js
@@ -1,15 +1,25 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
+import classes from "./Header.module.css";
+
 export default function Header() {
   return (
-        <header>
+        <header className={classes.header}>
             <Link to="/">
-                <div>MoveIt</div>
+                <div><h1>MoveIt</h1></div>
             </Link>
             <nav>
                 <ul>
-                    
+                    <li>
+                        <Link to="/">Home</Link>
+                    </li>
+                    <li>
+                        <Link to="/favorites">Favorites</Link>
+                    </li>
+                    <li>
+                        <Link to="/search">Search</Link>
+                    </li>
                 </ul>
             </nav>
         </header>
