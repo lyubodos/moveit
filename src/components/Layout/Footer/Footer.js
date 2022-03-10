ae1640ca17482b2baea6ab3e8d76fce6474f69fe
Vasilev <Lyubomir.Vasilev@adesso.bg>
2022-03-10 16:44:50 +0100

    Footer component add
    A basic Footer component was added.
    
    Basic implementation for the UI.

---
+++ src/components/Layout/Footer/Footer.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+import classes from "./Footer.module.css";
+
+export default function () {
+  return (
+      <footer>
+    <p className={classes.footer_para}>powered by <strong>MoveIt</strong> &copy;</p>
+
+      </footer>
+  )
+}
